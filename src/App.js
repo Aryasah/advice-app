774149f5ee41b5f2702d3bcd0740b00d5618b54d
Arya Sah <75942764+Aryasah@users.noreply.github.com>
2021-05-31 16:57:57 +0530

    final

--- src/App.js
+++ src/App.js
@@ -23,16 +23,32 @@ class App extends React.Component {
     render() {
         const {advice} =this.state;
         return (
+            <>
+            <header>
+		    <a href="#" id="logo" className="logo">Fun Advice</a>
+	        </header>
             <div className="app">
+            
                 <div className="card">
                 <h3 className="heading">{advice}</h3>
-                <button onClick={this.fetchAdvice} className="button">
-                    <span>
-                        GIVE SOME ADVICE! 
-                    </span>
+                <button onClick={this.fetchAdvice} className="btn">
+                <span className="noselect">GIVE ME ADVICE</span>
                 </button>
                 </div>
             </div>
+            <footer>
+            <ul>
+            <li><a href="https://www.facebook.com/arya.sah.98892"><i class="fa  fa-1x fa-facebook-official" aria-hidden="true"></i></a></li>
+			<li><a href="https://www.instagram.com/aryasah30/"><i class="fa fa-1x fa-linkedin" aria-hidden="true"></i></a></li>
+			<li><a href="https://www.linkedin.com/in/arya-sah-5100121b3/"><i class="fa fa-1x fa-instagram" aria-hidden="true"></i></a></li>
+		    </ul>
+            <ul id="second">
+			<li><a href="#">Made With ❤️</a></li>
+			<li><a href="#">&copy; Arya Sah</a></li>
+		    </ul>
+            </footer>
+            </>
+
         );
     }
 }
